e7dd6444c75279f32508a9337f8ca387e97c0c34
ColJops <jops24@o2.pl>
2023-07-12 00:25:13 +0200

    Użycie biblioteki Formik

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Checkout from './pages/checkout/checkout';
+//import Checkout from './pages/checkout/checkout';
+import FormikC from './pages/Formik';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Checkout />)
+root.render(<FormikC />)
+//root.render(<Checkout />)
